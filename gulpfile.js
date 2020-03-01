082cfe506041e8473d3656b53696590116cdbffe
Austin Bennett <austin@skaduusch.com>
2020-02-29 23:56:10 -0700

    Updated some linting rules, made some other related tweaks. Fixed more things

--- gulpfile.js
+++ gulpfile.js
@@ -9,55 +9,56 @@ const postcssPresetEnv = require('postcss-preset-env');
 const browserSync = require('browser-sync');
 
 const server = browserSync.create();
-const THEME_BASE = __dirname + '/wp-content/themes/sceraTheatre';
+const THEME_BASE = `${__dirname}/wp-content/themes/sceraTheatre`;
 
 function reload(done) {
-    server.reload();
-    done();
+	server.reload();
+	done();
 }
 function serve(done) {
-    server.init({
-        logSnippet: false,
-        server: false,
-        open: false,
-        reloadDelay: 100,
-        reloadDebounce: 100
-    });
-    done();
+	server.init({
+		logSnippet: false,
+		server: false,
+		open: false,
+		reloadDelay: 100,
+		reloadDebounce: 100,
+	});
+	done();
 }
 
 function clean() {
-    return del('main.css');
+	return del('main.css');
 }
 exports.clean = clean;
 
-function sass() {
-    return gulp
-        .src(`${THEME_BASE}/scss/main.scss`)
-        .pipe(sourcemaps.init())
-        .pipe(gulpSass().on('error', gulpSass.logError))
-        .pipe(
-            postcss([
-                postcssPresetEnv({
-                    stage: 2,
-                }),
-            ]),
-        )
-        .pipe(sourcemaps.write('.'))
-        .pipe(
-            gulp.dest(`${THEME_BASE}/css`, {
-                overwrite: true,
-            }),
-        )
-        .pipe(server ? server.reload({ stream: true }) : '');
+function scss() {
+	return gulp
+		.src(`${THEME_BASE}/scss/main.scss`)
+		.pipe(sourcemaps.init())
+		.pipe(gulpSass().on('error', gulpSass.logError))
+		.pipe(
+			postcss([
+				postcssPresetEnv({
+					stage: 2,
+					browsers: 'last 2 versions',
+				}),
+			]),
+		)
+		.pipe(sourcemaps.write('.'))
+		.pipe(
+			gulp.dest(`${THEME_BASE}/css`, {
+				overwrite: true,
+			}),
+		)
+		.pipe(server ? server.reload({ stream: true }) : '');
 }
-exports.sass = sass;
+exports.scss = scss;
 
 
 const watch = () => {
-    gulp.watch(`${THEME_BASE}/scss/*.scss`, sass);
-    gulp.watch([`${THEME_BASE}/*.php`, `${THEME_BASE}/js/*.js`], reload);
+	gulp.watch(`${THEME_BASE}/scss/*.scss`, scss);
+	gulp.watch([`${THEME_BASE}/*.php`, `${THEME_BASE}/js/*.js`], reload);
 };
 
-exports.default = gulp.series(clean, sass, serve, watch);
-exports.build = gulp.series(clean, sass);
+exports.default = gulp.series(clean, scss, serve, watch);
+exports.build = gulp.series(clean, scss);
--- wp-content/themes/sceraTheatre/footer.php
+++ wp-content/themes/sceraTheatre/footer.php
@@ -5,11 +5,9 @@
 		<i class="fab fa-facebook-f fa-lg icoLgPlus"></i>
 		<i class="fab fa-twitter fa-lg icoLgPlus"></i>			
 		<i class="fab fa-instagram fa-lg icoLgPlus"></i>			
-		<!-- <i class="fab fa-google-plus-square fa-3x"></i> -->
 		<!-- <i class="fab fa-youtube-square fa-3x"></i> -->
 	</div>
-	<!-- <footer>© 2019 • Austin Broadhead • <a href="http://austinbroadhead.com/">Designer Site</a></footer> -->
-	<footer>©2019 • SCERA</footer>
+	<footer>©<?php echo gmdate( 'Y' ); ?> • SCERA</footer>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
