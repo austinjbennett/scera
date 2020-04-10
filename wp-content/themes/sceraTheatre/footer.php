abb0600e931af01df01b4ae7f4f46b900959ffd3
DHarvard <dbh_1991@hotmail.com>
2020-04-09 21:42:24 -0600

    Fixed padding on social icons/Added style notes

--- wp-content/themes/sceraTheatre/footer.php
+++ wp-content/themes/sceraTheatre/footer.php
@@ -1,6 +1,7 @@
 <!-- </div> -->
 
-<div class="sponsors" id="keepOpen">
+<!-- Sponsors Section Style -->
+<div class="sponsors">
 	<h1>THANKS TO OUR SPONSORS</h1>
 	<div class="sponsorImages">
 		<div class="imageWrapper">
@@ -17,7 +18,10 @@
 	</div>
 </div>
 
+<!-- footerWrap Style -->
 <div class="footerWrap">
+
+	<!-- footerNav Style -->
 	<div class="footerNav">
 		<a>Movies</a>
 		<a>Events</a>
@@ -27,15 +31,19 @@
 		<a>About</a>
 		<a>Info</a>
 	</div>
+
+	<!-- socialIcons Style -->
 	<div class="socialIcons">
 		<i class="fab fa-facebook-f fa-lg icoLgPlus"></i>
 		<i class="fab fa-twitter fa-lg icoLgPlus"></i>			
 		<i class="fab fa-instagram fa-lg icoLgPlus"></i>
 		<i class="fab fa-youtube fa-lg icoLgPlus"></i>
 	</div>
-	<hr>
+
+	<!-- Copyright with year -->
 	<footer>©<?php echo gmdate( 'Y' ); ?> • SCERA</footer>
-	
+	<hr>
+	<!-- footerSearch Style -->
 	<div class='footerSearchWrap'>
 		<form id="searchForm" class=" searchArea" method="get" action="/">
 			<input class="sfield" type="search" name="s" placeholder="Search SCERA.org">
@@ -44,6 +52,7 @@
 	</div>
 </div>
 
+<!-- Scripts -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 <?php wp_footer(); ?>
