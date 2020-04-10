6c919eb6ff95e24cf5a1d80734e6299ef888b8e5
DHarvard <dbh_1991@hotmail.com>
2020-04-09 19:00:29 -0600

    Footer layout for small, medium, and large screens

--- wp-content/themes/sceraTheatre/footer.php
+++ wp-content/themes/sceraTheatre/footer.php
@@ -1,13 +1,47 @@
-	<!-- </div> -->
-	<!-- CONTACT US -->
+<!-- </div> -->
+
+<div class="sponsors" id="keepOpen">
+	<h1>THANKS TO OUR SPONSORS</h1>
+	<div class="sponsorImages">
+		<div class="imageWrapper">
+		
+		</div>
+
+		<div class="imageWrapper">
+		
+		</div>
+
+		<div class="imageWrapper">
+		
+		</div>
+	</div>
+</div>
+
 <div class="footerWrap">
+	<div class="footerNav">
+		<a>Movies</a>
+		<a>Events</a>
+		<a>Education</a>
+		<a>Auditions</a>
+		<a>Rentals</a>
+		<a>About</a>
+		<a>Info</a>
+	</div>
 	<div class="socialIcons">
 		<i class="fab fa-facebook-f fa-lg icoLgPlus"></i>
 		<i class="fab fa-twitter fa-lg icoLgPlus"></i>			
-		<i class="fab fa-instagram fa-lg icoLgPlus"></i>			
-		<!-- <i class="fab fa-youtube-square fa-3x"></i> -->
+		<i class="fab fa-instagram fa-lg icoLgPlus"></i>
+		<i class="fab fa-youtube fa-lg icoLgPlus"></i>
 	</div>
+	<hr>
 	<footer>©<?php echo gmdate( 'Y' ); ?> • SCERA</footer>
+	
+	<div class='footerSearchWrap'>
+		<form id="searchForm" class=" searchArea" method="get" action="/">
+			<input class="sfield" type="search" name="s" placeholder="Search SCERA.org">
+			<i class="fas fa-search fa-lg"></i>			
+		</form>
+	</div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
@@ -15,3 +49,4 @@
 <?php wp_footer(); ?>
 </body>
 </html>
+
