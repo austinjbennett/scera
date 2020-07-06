9cfea3736ac49a3e5ebc9ea070351fe158395341
Austin Bennett <austin@skaduusch.com>
2020-07-05 23:59:52 -0600

    Added tons of stuff. Homepage Carousel. Created Templates directory. Added some fonts (still in progress). Removed content images from repo. Refactored footer stuff. Added Swiper.js stuff. Moved most script and style import to functions.php enqueue_scripts.

--- wp-content/themes/sceraTheatre/functions.php
+++ wp-content/themes/sceraTheatre/functions.php
@@ -1,4 +1,7 @@
 <?php
+
+//flush_rewrite_rules();
+
 // REMOVE VISUAL EDITOR.
 //add_filter('user_can_richedit','__return_false', 50);
 
@@ -101,36 +104,49 @@ function create_post_type()
         )
     );
 
+    // Hero Slides
+    register_post_type(
+        'hero-slide',
+        array(
+            'labels' => array(
+                'name' => __('Hero Slides'),
+                'singular_name' => __('Hero Slide')
+            ),
+            'public' => true,
+            'has_archive' => true,
+        )
+    );
+
 }
 
 // ACF STUFF.
-if (function_exists('acf_add_options_page')) {
-    acf_add_options_page(
-        array(
-            'page_title' => 'Options',
-            'menu_title' => 'Options',
-            'menu_slug'  => 'options',
-            'capability' => 'edit_posts',
-            'redirect'   => false,
-        )
-    );
-
-    acf_add_options_sub_page(
-        array(
-            'page_title'  => 'Homepage',
-            'menu_title'  => 'Homepage',
-            'parent_slug' => 'options',
-        )
-    );
-
-    acf_add_options_sub_page(
-        array(
-            'page_title'  => 'Highlighted',
-            'menu_title'  => 'Highlighted',
-            'parent_slug' => 'options',
-        )
-    );
-}
+//if (function_exists('acf_add_options_page')) {
+//    acf_add_options_page(
+//        array(
+//            'page_title' => 'Options',
+//            'menu_title' => 'Options',
+//            'menu_slug'  => 'options',
+//            'capability' => 'edit_posts',
+//            'redirect'   => false,
+//        )
+//    );
+//
+//    acf_add_options_sub_page(
+//        array(
+//            'page_title'  => 'Homepage',
+//            'menu_title'  => 'Homepage',
+//            'parent_slug' => 'options',
+//        )
+//    );
+//
+//    acf_add_options_sub_page(
+//        array(
+//            'page_title'  => 'Highlighted',
+//            'menu_title'  => 'Highlighted',
+//            'parent_slug' => 'options',
+//        )
+//    );
+//}
 
 // STOP WP FROM ADDING P TAGS.
 remove_filter( 'the_content', 'wpautop' );
@@ -171,6 +187,22 @@ add_filter('excerpt_more', 'wpdocs_excerpt_more');
 //        )
 //    );
 //}
+function load_fonts() {
+    wp_enqueue_style( 'oxygenFont', 'https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Oxygen:wght@300;400;700&display=swap', false);
+	wp_enqueue_style( 'fontAwesome', 'https://use.fontawesome.com/releases/v5.7.1/css/all.css', false);
+}
+function load_swiper_files() {
+	if( is_front_page() ) {
+        wp_enqueue_style( 'swiperCSS', get_template_directory_uri() . '/vendor/swiperjs/swiper.min.css');
+        wp_enqueue_script( 'swiperJS', get_template_directory_uri() . '/vendor/swiperjs/swiper.min.js');
+    }
+}
+function load_main_script() {
+    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js');
+}
+//add_action('wp-enqueue_scripts', 'load_fonts');
+add_action('wp_enqueue_scripts', 'load_swiper_files');
+add_action('wp_enqueue_scripts', 'load_main_script');
 
 /**
  * DEVELOPMENT MODE ONLY
--- wp-content/themes/sceraTheatre/js/main.js
+++ wp-content/themes/sceraTheatre/js/main.js
@@ -1,25 +1,27 @@
-function fadeIn(element) {
-	let op = 0.1; // initial opacity
-	element.style.display = 'initial';
-	const timer = setInterval(() => {
-		if (op >= 1) {
-			clearInterval(timer);
-		}
-		element.style.opacity = op;
-		op += 0.1;
-	}, 10);
-}
-function fadeOut(element) {
-	let op = 1; // initial opacity
-	const timer = setInterval(() => {
-		if (op <= 0.1) {
-			clearInterval(timer);
-		}
-		element.style.opacity = op;
-		op -= 0.1;
-	}, 50);
-	element.style.display = 'none';
-}
+/* global $ */
+
+// function fadeIn(element) {
+// 	let op = 0.1; // initial opacity
+// 	element.style.display = 'initial';
+// 	const timer = setInterval(() => {
+// 		if (op >= 1) {
+// 			clearInterval(timer);
+// 		}
+// 		element.style.opacity = op;
+// 		op += 0.1;
+// 	}, 10);
+// }
+// function fadeOut(element) {
+// 	let op = 1; // initial opacity
+// 	const timer = setInterval(() => {
+// 		if (op <= 0.1) {
+// 			clearInterval(timer);
+// 		}
+// 		element.style.opacity = op;
+// 		op -= 0.1;
+// 	}, 50);
+// 	element.style.display = 'none';
+// }
 
 function toggleNav() {
 	const nav = document.getElementById('nav-wrapper');
@@ -35,84 +37,101 @@ function toggleNav() {
 		nav.classList.remove('open');
 	}
 }
-document.getElementById('nav-toggle').addEventListener('click', toggleNav);
+document.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('nav-toggle').addEventListener('click', toggleNav);
+	const mySwiper = new Swiper('.swiper-container', {
+		// Optional parameters
+		// direction: 'vertical',
+		loop: true,
+		height: 300,
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: 'true',
+		},
+		// breakpoints: {
+		// 	// when window width is >= 320px
+		// 	600: {
+		// 		height: 500,
+		// 	},
+		// 	// when window width is >= 480px
+		// 	992: {
+		// 		height: 600,
+		// 	},
+		// },
+	});
+});
 
-// Mobile Menu Button
-// function menuClick(x) {
-// 	x.classList.toggle('change');
+// // ASSIGN CLASSES TO PARENT AND SUB NAV
+// const navPars = document.querySelectorAll('.menu-item-has-children');
+// for (let i = 0; i < navPars.length; i += 1) {
+// 	const navPar = navPars[i];
+// 	navPar.classList.add('navParent');
+// 	const parA = navPar.querySelector('a');
+// 	// parA.setAttribute('onclick','toggleSubNav()');
+// 	// parA.onclick = toggleSubNav;
+// 	parA.classList.add('notPage');
+// 	const subUl = navPar.querySelector('.sub-menu');
+// 	// var subUl = navPar.querySelector('.dropdown-menu');
+// 	subUl.classList.add('subNav');
+// 	const subLi = subUl.querySelector('li');
+// 	subLi.classList.add('subLi');
 // }
-
-// ASSIGN CLASSES TO PARENT AND SUB NAV
-const navPars = document.querySelectorAll('.menu-item-has-children');
-for (let i = 0; i < navPars.length; i += 1) {
-	const navPar = navPars[i];
-	navPar.classList.add('navParent');
-	const parA = navPar.querySelector('a');
-	// parA.setAttribute('onclick','toggleSubNav()');
-	// parA.onclick = toggleSubNav;
-	parA.classList.add('notPage');
-	const subUl = navPar.querySelector('.sub-menu');
-	// var subUl = navPar.querySelector('.dropdown-menu');
-	subUl.classList.add('subNav');
-	const subLi = subUl.querySelector('li');
-	subLi.classList.add('subLi');
-}
 
 // var hbMenu = document.getElementById('hbMenu');
 // subNav toggle
-$('li.navParent').hover(function () {
-	if ($('#hbMenu:hidden')) {
-		console.log('is hide');
-		$(this).children('.subNav').slideToggle('fast');
-	}
-});
-$('li.navParent').click(function () {
-	$(this).children('.subNav').slideToggle('fast');
-});
+// $('li.navParent').hover(function () {
+// 	if ($('#hbMenu:hidden')) {
+// 		console.log('is hide');
+// 		$(this).children('.subNav').slideToggle('fast');
+// 	}
+// });
+// $('li.navParent').click(function () {
+// 	$(this).children('.subNav').slideToggle('fast');
+// });
 // function toggleSubNav(){
 // $('.subNav').slideToggle('normal');
 // };
 
 // Slide Toggle Mobile Nav
-$(() => {
-	$('#hbMenu').click(() => {
-		$('#myTopNav').slideToggle('normal');
-		console.log('we did it');
-	// $('.searchArea').slideToggle('normal');
-	});
-});
+// $(() => {
+// 	$('#hbMenu').click(() => {
+// 		$('#myTopNav').slideToggle('normal');
+// 		console.log('we did it');
+// 	// $('.searchArea').slideToggle('normal');
+// 	});
+// });
 
-const searchWrap = document.createElement('div');
-searchWrap.classList.add('searchWrap');
-searchWrap.innerHTML = '<form id="searchForm" class="form-inline searchArea" method="get" action="/">'
-	+ '<input class="sfield" type="search" name="s" placeholder="Search SCERA.org">'
-	+ '<i class="fas fa-search fa-lg"></i>'
-+ '</form>';
-$('#myTopNav').prepend(searchWrap);
-
-
-const myPag = $('.myPag');
-$('.aboveTitle').append(myPag);
-
-// LAZY LOAD GALLERY
-const lazyImgNodes = document.querySelectorAll('.lazyImg1');
-const lazyImgs = [];
-lazyImgNodes.forEach((laz) => {
-	lazyImgs.push(laz);
-});
-// console.log(lazyImgs);
-const inAdvnce = 0;
-let i;
-
-function lazyLoad() {
-	for (i = 0; i < lazyImgs.length; i++) {
-		if (lazyImgs[i].offsetTop < window.innerHeight + window.pageYOffset + inAdvnce) {
-			const lazNum = i + 1;
-			lazyImgs[i].src = `/03WordPress/lazyGal/lazy${lazNum}.jpg`;
-		}
-	}
-}
-lazyLoad();
-
-window.addEventListener('scroll', lazyLoad);
-window.addEventListener('resize', lazyLoad);
+// const searchWrap = document.createElement('div');
+// searchWrap.classList.add('searchWrap');
+// searchWrap.innerHTML = '<form id="searchForm" class="form-inline searchArea" method="get" action="/">'
+// 	+ '<input class="sfield" type="search" name="s" placeholder="Search SCERA.org">'
+// 	+ '<i class="fas fa-search fa-lg"></i>'
+// + '</form>';
+// $('#myTopNav').prepend(searchWrap);
+//
+// const myPag = $('.myPag');
+// $('.aboveTitle').append(myPag);
+//
+// // LAZY LOAD GALLERY
+// const lazyImgNodes = document.querySelectorAll('.lazyImg1');
+// const lazyImgs = [];
+// lazyImgNodes.forEach((laz) => {
+// 	lazyImgs.push(laz);
+// });
+// // console.log(lazyImgs);
+// const inAdvnce = 0;
+// let i;
+//
+// function lazyLoad() {
+// 	for (i = 0; i < lazyImgs.length; i++) {
+// 		if (lazyImgs[i].offsetTop < window.innerHeight + window.pageYOffset + inAdvnce) {
+// 			const lazNum = i + 1;
+// 			lazyImgs[i].src = `/03WordPress/lazyGal/lazy${lazNum}.jpg`;
+// 		}
+// 	}
+// }
+// lazyLoad();
+//
+// window.addEventListener('scroll', lazyLoad);
+// window.addEventListener('resize', lazyLoad);
