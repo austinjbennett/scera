5cb74d0d47f0bb754103782d7603b92ac6dfc1a7
DHarvard <dbh_1991@hotmail.com>
2020-07-29 13:38:28 -0600

    Working on education page

---
+++ wp-content/themes/sceraTheatre/templates/_educationTemplate.php
@@ -0,0 +1,111 @@
+<?php
+/*
+Template Name: Education Page
+*/
+get_header();
+?>
+
+<div class="education-page-container">
+
+    <section class="title-section">
+        <h2 class="title blue">Education</h2>
+    
+        <p class="text-section padding-none">SCERA has fun ways that kids, teens, adults, senior citizens and homeschoolers can get creative, develop a new talent and get excited about art! SCERA offers affordable tuition, wonderful instructors and a quality arts education in drama, dance, art, music and more. Programs offered year-round, from week long summer camps to fall and winter semester classes during the school year.</p>
+
+        <button class="scera-btn">Filter & Sort</button>
+    </section>
+
+    <section class="education-section bg-grey">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/art-club.png"" alt="">
+
+        <section class="class-outlook">
+            <div class="age-limit">
+                <h3>Age</h3>
+                <p>15+</p>
+            </div>
+
+            <div class="outlook-text-section">
+                <h3>Quarentine Art Club</h3>
+                <p class="outlook-text">April 16-May 29</p>
+                <p class="outlook-text">Various Times</p>
+                <p class="outlook-text">Streaming Link</p>
+                <p class="outlook-text">$5-$7 Per Class</p>
+            </div>
+
+        </section>
+
+        <p class="text-section">An art prompt using household materials. A secure link to our Zoom livestream will be sent to your email to join us on the day of the event. SIGN UP TWO HOURS BEFORE EVENTS! See times.
+        </p>
+
+        <h2 class="title blue center">Register</h2>
+        <h2 class="title blue center padding-none">For Online Class</h2>
+    </section>
+
+    <section class="education-section">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/ballet.png"" alt="">
+
+        <section class="class-outlook">
+            <div class="age-limit">
+                <h3>Age</h3>
+                <p>18+</p>
+            </div>
+
+            <div class="outlook-text-section">
+                <h3>Adult Ballet</h3>
+                <p class="outlook-text">May 5-June 23</p>
+                <p class="outlook-text">Tuesdays 7:30-8:30</p>
+                <p class="outlook-text">SCERA Center for the Arts</p>
+                <p class="outlook-text">$50 for 8 Week Session</p>
+            </div>
+
+        </section>
+
+        <p class="text-section">Join other adults in this eight week beginning course that will work through the basics and get you comfortable with the terms, steps, and abilities of a beginning ballet dancer. DANCE ATTIRE: Yoga pants/sweats, tee shirts/tank tops. and ballet shoes.
+        </p>
+
+        <h2 class="title blue center">Register</h2>
+        <h2 class="title blue center padding-none">For Online Class</h2>
+    </section>
+
+    <section class="education-section bg-grey">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/hip-hop.png"" alt="">
+
+        <section class="class-outlook">
+            <div class="age-limit">
+                <h3>Age</h3>
+                <p>16+</p>
+            </div>
+
+            <div class="outlook-text-section">
+                <h3>Aduklt Hip Hop</h3>
+                <p class="outlook-text">May 6-June 24</p>
+                <p class="outlook-text">Wednesday 7:30-8:30</p>
+                <p class="outlook-text">SCERA Center for the Arts</p>
+                <p class="outlook-text">$50 for 8 Week Session</p>
+            </div>
+
+        </section>
+
+        <p class="text-section">Join other adults in this eight week beginning course that will work through the basics and get you comfortable with the terms, steps, and abilities of a beginning hip hop dancer. Non-judgmental
+        </p>
+
+        <h2 class="title blue center">Register</h2>
+        <h2 class="title blue center padding-none">For Online Class</h2>
+    </section>
+
+    <section class ="center">
+        <button class="scera-btn">More Classes</button>
+    </section>
+
+    <section class="sponsors-section">
+		<h2 class="title blue">Thanks to our Sponsors</h2>
+
+		<div class="sponsor-images">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/orem-care.png" alt="">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/seal-of-transparency.png" alt="">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/utah-arts-council.png" alt="">
+		</div>
+	</section>
+</div>
+
+<?php get_footer(); ?>
