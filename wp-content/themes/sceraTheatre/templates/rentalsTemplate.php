73a998865b45919e3b8dbcb922b037a995aabb8b
DHarvard <dbh_1991@hotmail.com>
2020-08-08 10:08:41 -0600

    Rentals Page

---
+++ wp-content/themes/sceraTheatre/templates/rentalsTemplate.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: Rentals Page
+*/
+get_header();
+?>
+
+<div class="rentals-page-container">
+    <section class="title-section-center borderless-section">
+        <h2 class="title blue">Scera Rentals</h2>
+        <h3>Facilities Rentals</h3>
+
+        <p class="text-section">Are you looking for a place to hold a festival, meeting, event, party, recital, reception, workshop, class, concert, show or rehearsal? Interested in reserving an entire theater to watch a movie? SCERA may be able to help! SCERA facilities, rooms and theaters may be rented. Please call April Berlin at (801) 225-ARTS ext. 1011 to discuss pricing and availability or send an email to: april@scera.org.</p>
+
+        <a href="#" class="scera-btn">See Venues</a>
+    </section>
+
+    <section class="rentals-details-section bg-red border-section">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/rentals-building.png" alt="">
+        <h3>Costumes, Props, Scenery Rentals</h3>
+
+        <p class="text-section">The Prop and Costume Shop offers a year-round variety of costumes and prop rentals to individuals, groups, businesses, churches and theatrical organizations. We have hundreds of options of costumes props and scenery, large and small! The SCERA Prop, Costume and Scenery Shop is located in the brick building behind the parking lot east of the SCERA Center for the Arts. The entrance is located on the south side of the building. The Costume Shop rentals are BY APPOINTMENT ONLY.</p>
+
+        <div class="center">
+            <a href="#" class="scera-btn">Set Appointment</a>
+        </div>
+    </section>
+
+    <section class="visit-section borderless-section">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12175.311814626919!2d-111.70230536305183!3d40.279571870098984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x874d9a94a7a0f48f%3A0xf72b03aaec011118!2sSCERA%20Center%20for%20the%20Arts!5e0!3m2!1sen!2sus!4v1595549070393!5m2!1sen!2sus" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+        <section class="info">
+            <p class="text-section">COSTUME, PROP, AND SCENERY SHOP Location: 345 EAST 800 SOUTH</p>
+            <p class="text-section">Phone: (801) 225-ARTS</p>
+            <p class="text-section">Hours: 10am-6pm weekdays</p>
+        </section>
+
+        <p class="text-section">Send an Email to propshop@scera.org. Or costumeshop@scera.org with all your costume, prop, and scenery needs</p>
+    </section>
+</div>
+
+<?php get_footer(); ?>
