fd6e406bf5081396b24f0d62b7a3e45bbc95d2c2
DHarvard <dbh_1991@hotmail.com>
2020-08-11 17:51:28 -0600

    Basic Gift Card Page

---
+++ wp-content/themes/sceraTheatre/templates/giftCardTemplate.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Gift Card Page
+*/
+get_header();
+?>
+
+<div class="gift-card-page-container">
+    <section class="title-section borderless-section">
+        <h2 class="title Blue">Gift Cards</h2>
+
+        <p class="text-section padding">SCERA Gift Cards can be used at the SCERA Center for the Arts and the SCERA Shell Outdoor Theatre — simply hand the card to the cashier when you make a purchase! Gift cards can be used for movie, play and concert tickets, concessions, tuition, rentals and more!</p>
+
+        <p class="text-section padding">Gift cards are perfect for Christmas gifts, birthdays, graduation, anniversaries, student and employee incentives, and just about any special occasion. Gift cards may be purchased online below, by calling (801) 225-ARTS or in person at the SCERA Center for the Arts, 745 South State, Orem, open 10am-6pm weekdays and Saturdays from 12Noon-6pm.</p>
+
+        <p class="text-section">If you order gift cards online, please allow 24 hours to process your order and additional time for shipping. Each gift card will come with a gift card holder with places for TO, FROM, AMOUNT and MESSAGE. There are no refunds on gift card orders.</p>
+    </section>
+
+    <section class="bg-grey borderless-section">
+        <div class="center">
+            <a href="#" class="scera-btn">Purchase</a>
+        </div>
+    </section>
+</div>
+
+<?php get_footer(); ?>
